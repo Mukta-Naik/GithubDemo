241442c73ed3ca560634d7fe8a8248cd80173caf
mnaik <mnaik@xento.com>
2020-05-04 18:26:47 +0530

    Adding new demo2

--- demo2.php
+++ demo2.php
@@ -4,5 +4,7 @@ echo 'Hi';
 echo 'hi123';
 echo 'demo2 commit';
 echo 'Test with a new line';
+echo 'New line added in demo branch to be merged with storm branch';
+echo 'New line added in demo branch';
 }
 ?>
