d6e871fccad77f0d5828d509168395e3f5f3fc0b
mnaik <mnaik@xento.com>
2020-05-08 10:12:32 +0530

    Adding new demo2

--- demo2.php
+++ demo2.php
@@ -7,5 +7,6 @@ echo 'Test with a new line';
 echo 'New line added in demo branch to be merged with storm branch';
 echo 'New line added in demo branch';
 echo 'Add new line to demo2 after push from demo2 branch';
+echo 'add a logic in payreview branch';
 }
 ?>
